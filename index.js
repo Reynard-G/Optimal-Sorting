9f2998192c64da56b9d52f9f2b639665c20f03ea
Reynard-G <reynard.ean.gunawan@gmail.com>
2022-09-09 19:56:19 -0500

    New Algorithm, ~.7s/10000

--- index.js
+++ index.js
@@ -11,33 +11,33 @@ var rl = readline.createInterface({
 rl.on('line', function(line){
     if(line.replace(/\n/, "").length == 0) {
 	rl.close();
-	sortlist(arr)
+	sort(arr)
     } else arr.push(line)
 })
-    
+
 /*
 fs.readFile('text.txt', function(err, data) {
     if(err) throw err;
     arr = data.toString().split("\n");
-    sortlist(arr)
+    sort(arr)
 });
 */
 
-function swap(list, i, j) {
-    [list[i], list[j]] = [list[j], list[i]];
-    return list;
-}
+function sort(data) {
+    let min;
 
-function sortlist(unsorted) {
-    var count = unsorted.length;
-    
-    for (var i = 0; i < unsorted.length; i++) {
-	for (var j = i + 1; j < count; j++) {
-	    if (unsorted[i] > unsorted[j]) {
-		swap(unsorted, i, j);
+    for (let i = 0; i < data.length; i++) {
+	min = i;
+	for (let j = i + 1; j < data.length; j++) {
+	    if (data[j] < data[min]) {
+		min = j;
 	    }
 	}
-	count -= 1
-	console.log(unsorted[i])
+	if (min != i) {
+	    [data[i], data[min]] = [data[min], data[i]];
+	}
     }
-}
+    arr.forEach(function(element) {
+	console.log(element)
+    });
+};
